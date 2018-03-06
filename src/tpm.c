d524b25e0f89b3724ae5f0a8882625c1fc794620
Jernej Turnšek <J_Turnsek@iskraemeco.si>
2018-03-06 13:58:00 +0100

    Bug fix

--- src/tpm.c
+++ src/tpm.c
@@ -516,7 +516,7 @@ TPM2_RC tpm_rsa_sign(TSS2_SYS_CONTEXT *sapi_context, TPMI_DH_OBJECT handle, unsi
 
   TPM2B_DIGEST digest = { .size = digestSize };
   // Remove OID from hash if provided
-  memcpy(digest.buffer, hash - digestSize + hash_length, hash_length);
+  memcpy(digest.buffer, hash - digestSize + hash_length, digestSize/*hash_length*/);
 
   TSS2_RC rval = TSS2_RETRY_EXP(Tss2_Sys_Sign(sapi_context, handle, &sessions_data, &digest, &scheme, &validation, signature, &sessions_data_out));
 
