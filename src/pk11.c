597511259423f37872a15b5345570f299e208ef5
jturnsek <J_Turnsek@iskraemeco.si>
2018-04-04 12:59:56 +0200

    Working on CreateObject again

--- src/pk11.c
+++ src/pk11.c
@@ -551,7 +551,7 @@ CK_RV C_CreateObject(CK_SESSION_HANDLE hSession, CK_ATTRIBUTE_PTR pTemplate, CK_
           char filename[256];
           int j;
           for (j = 0; j < pTemplate[i].ulValueLen; j++) {
-            print_log(VERBOSE, "C_CreateObject: id byte = %x", pTemplate[i].pValue + j);
+            print_log(VERBOSE, "C_CreateObject: id byte = %x", *((unsigned char*)pTemplate[i].pValue + j));
             sprintf((char*) filename + j * 2, "%02X", (unsigned char)pTemplate[i].pValue + j);
           }
           filename[j] = 0;
